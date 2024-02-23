19024049002583d2a28242df1a4339806a1b8333
maxmib <30944505+maxmib@users.noreply.github.com>
2024-02-23 15:09:04 +0800

    Update updater.py

--- custom_components/broadlink_extra/updater.py
+++ custom_components/broadlink_extra/updater.py
@@ -7,7 +7,7 @@ import logging
 from .broadlinkdiy.exceptions import AuthorizationError, BroadlinkException
 
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from homeassistant.util import dt
+from homeassistant.util import dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,6 +18,7 @@ def get_update_manager(device):
         "A1": BroadlinkA1UpdateManager,
         "A2": BroadlinkA2UpdateManager,
         "BG1": BroadlinkBG1UpdateManager,
+        "HYS": BroadlinkThermostatUpdateManager,
         "LB1": BroadlinkLB1UpdateManager,
         "LB2": BroadlinkLB1UpdateManager,
         "MP1": BroadlinkMP1UpdateManager,
@@ -66,7 +67,7 @@ class BroadlinkUpdateManager(ABC):
 
         except (BroadlinkException, OSError) as err:
             if self.available and (
-                dt.utcnow() - self.last_update > self.SCAN_INTERVAL * 3
+                dt_util.utcnow() - self.last_update > self.SCAN_INTERVAL * 3
                 or isinstance(err, (AuthorizationError, OSError))
             ):
                 self.available = False
@@ -78,17 +79,16 @@ class BroadlinkUpdateManager(ABC):
                 )
             raise UpdateFailed(err) from err
 
-        else:
-            if self.available is False:
-                _LOGGER.warning(
-                    "Connected to %s (%s at %s)",
-                    self.device.name,
-                    self.device.api.model,
-                    self.device.api.host[0],
-                )
-            self.available = True
-            self.last_update = dt.utcnow()
-            return data
+        if self.available is False:
+            _LOGGER.warning(
+                "Connected to %s (%s at %s)",
+                self.device.name,
+                self.device.api.model,
+                self.device.api.host[0],
+            )
+        self.available = True
+        self.last_update = dt_util.utcnow()
+        return data
 
     @abstractmethod
     async def async_fetch_data(self):
@@ -184,7 +184,7 @@ class BroadlinkBG1UpdateManager(BroadlinkUpdateManager):
 
 class BroadlinkSP4UpdateManager(BroadlinkUpdateManager):
     """Manages updates for Broadlink SP4 devices."""
-    SCAN_INTERVAL = timedelta(seconds=5)  
+
     async def async_fetch_data(self):
         """Fetch data from the device."""
         return await self.device.async_request(self.device.api.get_state)
@@ -196,3 +196,11 @@ class BroadlinkLB1UpdateManager(BroadlinkUpdateManager):
     async def async_fetch_data(self):
         """Fetch data from the device."""
         return await self.device.async_request(self.device.api.get_state)
+
+
+class BroadlinkThermostatUpdateManager(BroadlinkUpdateManager):
+    """Manages updates for thermostats with Broadlink DNA."""
+
+    async def async_fetch_data(self):
+        """Fetch data from the device."""
+        return await self.device.async_request(self.device.api.get_full_status)
